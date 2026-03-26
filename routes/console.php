2edc9b26323a2a5cb6295c62ebd844b590f9bb1c
Sergiu Balaban <sergiu.balaban92@gmail.com>
2026-03-26 07:08:15 +0200

    Add tail artisan command

--- routes/console.php
+++ routes/console.php
@@ -2,7 +2,24 @@
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Symfony\Component\Process\Process;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+Artisan::command(
+    'tail {file=storage/logs/laravel.log : A path to the file being tailed. }
+        {--only= : Only show lines containing this string. }
+    ',
+    function () {
+        $command = 'tail -f "$FILE" | grep "$ONLY"';
+
+        Process::fromShellCommandline($command)
+            ->setTty(true)
+            ->setTimeout(null)
+            ->run(null, [
+                'FILE' => $this->argument('file'),
+                'ONLY' => $this->option('only'),
+            ]);
+    });
