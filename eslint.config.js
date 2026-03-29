7ac2f21b7268ce74a30b314bd8392423da5e9830
Sergiu Balaban <sergiu.balaban92@gmail.com>
2026-03-29 09:32:54 +0300

    Clear code with eslint.config.js

--- eslint.config.js
+++ eslint.config.js
@@ -1,9 +1,24 @@
-import js from "@eslint/js";
-import globals from "globals";
-import pluginVue from "eslint-plugin-vue";
-import { defineConfig } from "eslint/config";
+import js from '@eslint/js';
+import tsParser from '@typescript-eslint/parser';
+import pluginVue from 'eslint-plugin-vue';
+import { defineConfig } from 'eslint/config';
+import globals from 'globals';
+import vueParser from 'vue-eslint-parser';
 
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs,vue}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.browser } },
-  pluginVue.configs["flat/essential"],
+    {
+        files: ['**/*.{js,mjs,cjs,vue}'],
+        plugins: { js },
+        extends: ['js/recommended'],
+        languageOptions: {
+            globals: globals.browser,
+            parser: vueParser,
+            parserOptions: {
+                parser: tsParser,
+                ecmaVersion: 'latest',
+                sourceType: 'module',
+            },
+        },
+    },
+    pluginVue.configs['flat/essential'],
 ]);
